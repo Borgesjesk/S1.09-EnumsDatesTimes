e912e4f665cffe1ae587f9c811dcf7f171fe8983
BORGES <jessborgesb@gmail.com>
2026-03-15 15:27:53 +0100

    feat: add Appointment class for N2 agenda exercise

--- src/main/java/cat/itacademy/s1_09/n2/Appointment.java
+++ src/main/java/cat/itacademy/s1_09/n2/Appointment.java
@@ -1,4 +1,22 @@
 package cat.itacademy.s1_09.n2;
 
+import java.time.LocalDateTime;
+
 public class Appointment {
+
+    private final String title;
+    private final LocalDateTime dateTime;
+
+    public Appointment(String title, LocalDateTime dateTime) {
+        this.title = title;
+        this.dateTime = dateTime;
+    }
+
+    public String getTitle() { return title; }
+    public LocalDateTime getDateTime() { return dateTime; }
+
+    @Override
+    public String toString() {
+        return "Appointment{title='" + title + "', dateTime=" + dateTime + "}";
+    }
 }
